d2787d4f4c1a431f26f5a3c1b6db5cea65c5f950
Ritesh <itsmeriteshpatidar@gmail.com>
2025-02-21 14:40:01 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,90 @@
+//NOTE value and variable
+
+(value) => data;
+// // data store in the variables
+// data =>
+//NOTE = Data types
+//NOTE PRIMTIVE DATA TYPES
+//NOTE 7 primitive data types
+
+//TODO NUMBER => 0-9
+//TODO STRING => 'A-z' '0-9'  // STRING => '' , ""  ,    ``
+//TODO BOOLEAN => TRUE OR FALSE  isJSFun => true
+//TODO UNDEFINED =>
+//TODO NULL =>
+//TODO BIGINT < SYMBOL
+
+//NON-PRIMITIVE => array , object , fn ;
+
+//PRIMITIVE
+
+//NOTE KEYWORDS
+//let , const , var
+
+let firstname = 'ritesh';
+console.log(firstname);
+//NOTE REASSIGN , REDECLARATION
+firstname = 'AMAN';
+console.log(firstname);
+
+//NOTE REDECLARATION
+// let firstname = 'piyush'
+
+//NOTE VARIABLE DECALRED WITH LET KEYWORD CAN BE ✅REASSIGN BUT CANNOT BE ❌REDECLARED
+
+const age = '🧑‍🦲' ;
+//REASSIGN
+// age = 26 ;
+// console.log(age)
+//REDECLARATION
+// const age = 37
+
+
+//NOTE VAR => (LEGACY KEYWORD USED IN OLDER VERSION)
+
+var isJSFun = true ;
+
+//reassign
+isJSFun = false ;
+console.log(isJSFun)
+
+//redeclaration ;
+var isJSFun = true ;
+console.log(isJSFun)
+
+
+//VARIABLE NAMING IN JS 
+
+//MEANING FULL NAME 
+//js camelCase => 
+
+
+const companyName = 'META' 
+const phoneNumber = 8989898989
+const haveYouPassInExam = true
+let collegeName ;
+console.log(collegeName)
+console.log(typeof collegeName)
+collegeName = 'JEC'
+console.log(collegeName)
+
+let channel = null
+console.log(channel)
+// 💡 POINTS TO REMEMBER THE TYPE OF NULL IS OBJECT
+console.log(typeof channel)
+// => 'string'
+
+//NOTE typeof operator
+const type = typeof companyName
+console.log(type) ;
+console.log(typeof phoneNumber)
+console.log(typeof haveYouPassInExam)
+
+
+// 💡💡💡 IMP CONCEPTS TILL LINE 84
+//NOTE UNDEFINED VS NULL
+//NOTE LET < CONST < VAR MAIN DIFFERENCE
+//NOTE REASSIGN AND REDECLARATION
+//NOTE VARIABLE INITIALIZATION 
+//NOTE TYPEOF OPRETOR TO CHECK THE TYPE OF DATA STORED IN A VARIABLE
+

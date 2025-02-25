22fa9839a4061cf7752d866f202fd6c00365d220
Ritesh <itsmeriteshpatidar@gmail.com>
2025-02-25 12:52:20 +0530

    added code till funcitons

---
+++ DAY03/script.js
@@ -0,0 +1,46 @@
+
+
+
+//FUNCTIONS 
+const result = greet() ;
+console.log(result)
+//FUNCTION DECLARATION 
+function greet(){
+    return 'hello'
+}
+
+//FUNCTION EXPRESSION 
+// console.log(sum(2,3))
+
+const sum = function(a,b){
+return  a+b ;
+}
+
+//arrow function 
+//single line
+//multiline 
+
+const arrowFn = a => a
+console.log(arrowFn(4))
+
+const anotherSum = () => {
+    const a = 5 ;
+    const b= 5 ;
+ return a+b
+}
+console.log(anotherSum()) ;
+
+console.log('hello')
+
+
+//1.git init to initialize git in the current folder its started to track changes in you current working directory
+
+//git status => used to check the untracked files
+
+//Staging area => 
+
+//git add script.js
+
+//git commit -m "commit-message"
+
+//a code version is created in the localrepo
--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //NOTE value and variable
-
 (value) => data;
 // // data store in the variables
 // data =>
@@ -32,59 +31,126 @@ console.log(firstname);
 
 //NOTE VARIABLE DECALRED WITH LET KEYWORD CAN BE ✅REASSIGN BUT CANNOT BE ❌REDECLARED
 
-const age = '🧑‍🦲' ;
+const age = '🧑‍🦲';
 //REASSIGN
 // age = 26 ;
 // console.log(age)
 //REDECLARATION
 // const age = 37
 
-
 //NOTE VAR => (LEGACY KEYWORD USED IN OLDER VERSION)
 
-var isJSFun = true ;
+var isJSFun = true;
 
 //reassign
-isJSFun = false ;
-console.log(isJSFun)
+isJSFun = false;
+console.log(isJSFun);
 
 //redeclaration ;
-var isJSFun = true ;
-console.log(isJSFun)
+var isJSFun = true;
+console.log(isJSFun);
 
+//VARIABLE NAMING IN JS
 
-//VARIABLE NAMING IN JS 
+//MEANING FULL NAME
+//js camelCase =>
 
-//MEANING FULL NAME 
-//js camelCase => 
+const companyName = 'META';
+const phoneNumber = 8989898989;
+const haveYouPassInExam = true;
+let collegeName;
+console.log(collegeName);
+console.log(typeof collegeName);
+collegeName = 'JEC';
+console.log(collegeName);
 
-
-const companyName = 'META' 
-const phoneNumber = 8989898989
-const haveYouPassInExam = true
-let collegeName ;
-console.log(collegeName)
-console.log(typeof collegeName)
-collegeName = 'JEC'
-console.log(collegeName)
-
-let channel = null
-console.log(channel)
+let channel = null;
+console.log(channel);
 // 💡 POINTS TO REMEMBER THE TYPE OF NULL IS OBJECT
-console.log(typeof channel)
+console.log(typeof channel);
 // => 'string'
 
 //NOTE typeof operator
-const type = typeof companyName
-console.log(type) ;
-console.log(typeof phoneNumber)
-console.log(typeof haveYouPassInExam)
+const type = typeof companyName;
+console.log(type);
+console.log(typeof phoneNumber);
+console.log(typeof haveYouPassInExam);
 
-
-// 💡💡💡 IMP CONCEPTS TILL LINE 84
+// 💡💡IMP CONCEPTS TILL LINE 84
 //NOTE UNDEFINED VS NULL
 //NOTE LET < CONST < VAR MAIN DIFFERENCE
 //NOTE REASSIGN AND REDECLARATION
-//NOTE VARIABLE INITIALIZATION 
+//NOTE VARIABLE INITIALIZATION
 //NOTE TYPEOF OPRETOR TO CHECK THE TYPE OF DATA STORED IN A VARIABLE
 
+//GITHUB LINK => https://github.com/riteshpatidar08/jec-js
+
+//NOTE OPERATORS
+
+//NOTE AIRTHMATIC OPERATORs
+2 ** 2; //EXPONENTIAL
+// % modules
+// / divide
+
+//NOTE ASSIGNMENT OPERATORs
+let x = 5;
+x = x + 5;
+x += 5;
+
+//NOTE COMPARISION OPERATOR
+// >= , <= , >  ,<
+//💫TODO  UNIQUE IN THE JS-
+
+x == x; //LOOSE EQUALITY
+x === x; //STRICT EQUALITY
+// 5 != '5'  5 !== '5'
+//NOTE LOGICAL OPERATORs
+// &&  , || , !
+
+//NOTE INTERVIEW >
+//NOTE WHAT IS THE DIFFERENCE BETWEEN LOOSE EQUALITY AND STRICT EQUALITY ?
+
+//string interpolation or template string ;
+//my name is ritesh
+const firstName = 'ritesh';
+const intro = 'my' + 'name' + 'is' + firstName;
+console.log(intro);
+const newIntro = `my name is ${firstName}`;
+console.log(newIntro);
+
+//falsy values and truthy values
+
+// 0,'',null,undefined , -0
+// !0 and !'' => truthy
+
+if (' ') {
+  console.log('truthy value');
+} else {
+  console.log('falsy value');
+}
+
+//interaction function
+// alert('hello');
+// const agee = prompt('enter your age');
+console.log(typeof agee);
+
+console.log('number...', Number(age));
+console.log(Boolean('true'));
+console.log(String(true));
+
+// const isJsFun = confirm('are you learing js');
+// console.log(isJsFun);
+
+//NOTE TYPE CONVERSION AND COERSION
+const num = '5';
+const strNum = +num;
+console.log(typeof strNum);
+console.log(laptop);
+
+//case aeiou => vowels //consonent
+
+// switch(){
+//     case :
+// }
+
+//NOTE DAY 3
